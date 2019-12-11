ac8c4ecaa5e537ed924b3a527d0683954eb9b153
Mark T. Holder <mtholder@gmail.com>
2019-12-11 13:23:23 -0800

    comments

--- opentree/__init__.py
+++ opentree/__init__.py
@@ -17,25 +17,34 @@ class OTClientError(Exception):
 
 
 class OTWebServiceWrapper(WebServiceWrapperRaw):
+    """This class provides a wrapper to the Open Tree of Life web service methods.
+    Actual HTTP calls are handled by methods implemented in the base class for clarity of this code.
+    API method calls will be mappable to methods in this class. The methods implemented here do argument checking
+    and conversion of the returned JSON to more usable objects.
+    """
     def __init__(self, api_endpoint):
         WebServiceWrapperRaw.__init__(self, api_endpoint)
         self.to_object_converter = get_object_converter('dendropy')
 
     def tree_of_life_induced_subtree(self, node_ids=None, ott_ids=None, label_format="name_and_id"):
         d = {"label_format": label_format.lower().strip()}
-        if node_ids or ott_ids:
-            if node_ids and ott_ids:
-                raise ValueError("Only one of `node_ids` and `ott_ids` can be specified")
-            if node_ids:
-                d["node_ids"] = [str(i) for i in node_ids]
-            else:
-                d["ott_ids"] = [int(i) for i in ott_ids]
+        if not (node_ids or ott_ids):
+            raise ValueError("Either node_ids or ott_ids must be provided")
+        if node_ids:
+            d["node_ids"] = [str(i) for i in node_ids]
+        if ott_ids:
+            d["ott_ids"] = [int(i) for i in ott_ids]
         resp_dict = self._call_api('tree_of_life/induced_subtree', data=d)
         newick = resp_dict['newick']
         return self.to_object_converter.tree_from_newick(newick, suppress_internal_node_taxa=True)
 
 
 class OpenTree(object):
+    """This class is intended to provide a high-level wrapper for interaction with OT web services and data.
+    The method names are intended to be clear to a wide variety of users, rather than (necessarily matching
+    the API calls directly.
+
+    """
     def __init__(self):
         self._api_endpoint = 'production'
         self._ws = None
