7f21d7c75b5afed8a3e142fcc7164a99f6cd4667
Mark T. Holder <mtholder@gmail.com>
2019-12-22 18:40:09 -0600

    minor refactor

--- opentree/__init__.py
+++ opentree/__init__.py
@@ -7,39 +7,12 @@ from .ws_wrapper import (OTWebServicesError,
                          WebServiceRunMode,
                          WebServiceWrapper,
                          )
+from .ot_ws_wrapper import OTWebServiceWrapper
 import logging
 import argparse
 import atexit
 import sys
 import os
-
-
-class OTWebServiceWrapper(WebServiceWrapper):
-    """This class provides a wrapper to the Open Tree of Life web service methods.
-    Actual HTTP calls are handled by methods implemented in the base class for clarity of this code.
-    API method calls will be mappable to methods in this class. The methods implemented here do argument checking
-    and conversion of the returned JSON to more usable objects.
-    """
-
-    def __init__(self, api_endpoint, run_mode=WebServiceRunMode.RUN):
-        WebServiceWrapper.__init__(self, api_endpoint, run_mode=run_mode)
-        self.to_object_converter = get_object_converter('dendropy')
-
-    def tree_of_life_induced_subtree(self, node_ids=None, ott_ids=None, label_format="name_and_id"):
-        d = {"label_format": label_format.lower().strip()}
-        if not (node_ids or ott_ids):
-            raise ValueError("Either node_ids or ott_ids must be provided")
-        if node_ids:
-            d["node_ids"] = [str(i) for i in node_ids]
-        if ott_ids:
-            d["ott_ids"] = [int(i) for i in ott_ids]
-        return self._call_api('tree_of_life/induced_subtree', data=d, demand_success=False)
-
-    def taxonomy_about(self):
-        return self._call_api('taxonomy/about')
-
-    def tree_of_life_about(self):
-        return self._call_api('tree_of_life/about')
 
 
 class OpenTree(object):
---
+++ opentree/ot_ws_wrapper.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+__version__ = "0.0.1"  # sync with setup.py
+
+from .object_conversion import get_object_converter
+from .ws_wrapper import (WebServiceRunMode,
+                         WebServiceWrapper,
+                         )
+
+
+class OTWebServiceWrapper(WebServiceWrapper):
+    """This class provides a wrapper to the Open Tree of Life web service methods.
+    Actual HTTP calls are handled by methods implemented in the base class for clarity of this code.
+    API method calls will be mappable to methods in this class. The methods implemented here do argument checking
+    and conversion of the returned JSON to more usable objects.
+    """
+
+    def __init__(self, api_endpoint, run_mode=WebServiceRunMode.RUN):
+        WebServiceWrapper.__init__(self, api_endpoint, run_mode=run_mode)
+        self.to_object_converter = get_object_converter('dendropy')
+
+    def tree_of_life_induced_subtree(self, node_ids=None, ott_ids=None, label_format="name_and_id"):
+        d = {"label_format": label_format.lower().strip()}
+        if not (node_ids or ott_ids):
+            raise ValueError("Either node_ids or ott_ids must be provided")
+        if node_ids:
+            d["node_ids"] = [str(i) for i in node_ids]
+        if ott_ids:
+            d["ott_ids"] = [int(i) for i in ott_ids]
+        return self._call_api('tree_of_life/induced_subtree', data=d, demand_success=False)
+
+    def taxonomy_about(self):
+        return self._call_api('taxonomy/about')
+
+    def tree_of_life_about(self):
+        return self._call_api('tree_of_life/about')
