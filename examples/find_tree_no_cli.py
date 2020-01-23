e7aadc10043fdea68142a2a860525be9c528cea8
Emily Jane McTavish <ejmctavish@gmail.com>
2020-01-23 15:22:39 -0800

    pretty up

--- examples/find_tree_no_cli.py
+++ examples/find_tree_no_cli.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from opentree import OpenTree
 
 
@@ -8,13 +9,13 @@ print(json.dumps(output.response_dict, indent=2, sort_keys=True))
 
 study_id = "ot_864"
 tree_id = "tree1"
-output = OT.get_tree(study_id, tree_id, tree_format=".tre", label_format="ot:ottid")
+
+output = OT.get_tree(study_id, tree_id)
 tre = output.response_dict[tree_id]
-print(tre)
+sys.stdout.write("This tree returns fine, but isn't very meaningful due to lack of otu labels:\n\n {t}\n\n".format(t=tre))
 
-## Compare to Dev
 
-#devot = OpenTree(api_endpoint="dev")
-#devout = devot.find_trees(search_property="ot:branchLengthMode", value="ot:time")
+output = OT.get_tree(study_id, tree_id, tree_format=".tre", label_format="ot:ottid")
+sys.stdout.write("The url looks fine, but the response dict can't be decoded\n")
+tre = output.response_dict[tree_id]
 
-##compare results or whatever
