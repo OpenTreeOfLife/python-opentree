a7f1dc540867f6a1ba913ed6610dbc687fbd5451
Emily Jane McTavish <ejmctavish@gmail.com>
2020-01-28 11:51:54 -0800

    example walking through dict and getting trees

---
+++ examples/get_tree_no_cli.py
@@ -0,0 +1,18 @@
+import json
+from opentree import OT
+# find all trees ids that are chronograms
+output = OT.find_trees('ot:time', search_property = 'ot:branchLengthMode') 
+
+tree_dict = output.response_dict
+
+for study in tree_dict['matched_studies']:
+    study_id = study['ot:studyId']
+    for tree in study['matched_trees']:
+        tree_id = tree['ot:treeId']
+        assert(study_id == tree['ot:studyId'])
+        print("Study {} tree {}".format(study_id,tree_id))
+        output_tree = OT.get_tree(study_id, tree_id, tree_format=".tre", label_format="ot:ottid")
+        tre = output_tree._response_obj.text
+        print(tre)
+
+
