0b0d96e3f68e3bf7077964685a85e78b05e11986
Mark T. Holder <mtholder@gmail.com>
2020-02-21 14:11:21 -0600

    hooks for having an optional, more extensive test dir

--- examples/about.py
+++ examples/about.py
@@ -3,14 +3,22 @@
 import sys
 from opentree import OTCommandLineTool
 
-cli = OTCommandLineTool(usage='Display taxonomy and synthetic tree information '
+def main(arg_list, out):
+    cli = OTCommandLineTool(usage='Display taxonomy and synthetic tree information '
                               'returned by the "about" API calls.')
-OT = cli.parse_cli()[0]
+    OT = cli.parse_cli(arg_list)[0]
+    about = OT.about()
 
-about = OT.about()
-for k in about.keys():
-    call_record = about[k]
-    if call_record:
-        print(k)
-        call_record.write_response(sys.stdout)
-        print('')
+    if out is None:
+        return 0
+    for k in about.keys():
+        call_record = about[k]
+        if call_record:
+            print(k)
+            call_record.write_response(out)
+            print('')
+    return 0
+
+if __name__  == '__main__':
+    rc = main(sys.argv, sys.stdout)
+    sys.exit(rc)
