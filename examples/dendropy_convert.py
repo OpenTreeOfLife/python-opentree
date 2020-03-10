e16933fb2c9e9b3d24d0db5bd90fe4438f079ec7
Emily Jane McTavish <ejmctavish@gmail.com>
2020-03-10 12:10:51 -0700

    fixed example

--- examples/dendropy_convert.py
+++ examples/dendropy_convert.py
@@ -9,7 +9,10 @@ study_id = 'ot_1877'
 
 tree_id = 'tree3' # "tree1"
 study = OT.get_study(study_id)
-study_nexson = study.response_dict
+study_nexson = study.response_dict['data']
 
 
-DC.tree_from_nexson(study_nexson, tree_id)
+tree_obj = DC.tree_from_nexson(study_nexson, tree_id)
+
+tree_obj.internal_node_ages()
+
