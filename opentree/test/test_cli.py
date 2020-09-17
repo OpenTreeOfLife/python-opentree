f2bf27b23e45a4089b57a4e09b8ec7754bfc3de5
Emily Jane McTavish <ejmctavish@gmail.com>
2020-09-17 15:40:45 -0700

    simplify

--- opentree/test/test_cli.py
+++ opentree/test/test_cli.py
@@ -1,15 +1,12 @@
 import unittest
-import sys
 import subprocess
 from opentree import OT
 
 
 class TestCLI(unittest.TestCase):
-    def test_python_versions(self):
-        assert 'python' in sys.executable, sys.executable
     def test_diagnose_solution(self):
         url = 'https://tree.opentreeoflife.org/curator/study/view/ot_1344?tab=trees&tree=Tr105486&node=Tn16531763'
-        p = subprocess.run(["/home/travis/virtualenv/python3.7.1/bin/python", "examples/diagnose_solution_for.py",
+        p = subprocess.run(["./examples/diagnose_solution_for.py",
                             "--ott-id", "1066581"],  
                             stdout=subprocess.PIPE, 
                             input='\n'.encode()) #input keeps it from hanging waiting for a subproblem id
