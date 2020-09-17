16714dbd2481a565380edd68b53d69a58dc00e1d
Emily Jane McTavish <ejmctavish@gmail.com>
2020-09-17 15:34:59 -0700

    excutable

--- opentree/test/test_cli.py
+++ opentree/test/test_cli.py
@@ -9,7 +9,7 @@ class TestCLI(unittest.TestCase):
         assert 'python' in sys.executable, sys.executable
     def test_diagnose_solution(self):
         url = 'https://tree.opentreeoflife.org/curator/study/view/ot_1344?tab=trees&tree=Tr105486&node=Tn16531763'
-        p = subprocess.run(["~/virtualenv/python3.7/bin/python", "examples/diagnose_solution_for.py",
+        p = subprocess.run(["/home/travis/virtualenv/python3.7.1/bin/python", "examples/diagnose_solution_for.py",
                             "--ott-id", "1066581"],  
                             stdout=subprocess.PIPE, 
                             input='\n'.encode()) #input keeps it from hanging waiting for a subproblem id
