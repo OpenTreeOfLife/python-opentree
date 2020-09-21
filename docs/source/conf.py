fc8d3548f1b67c5d3f2d4a60cb947c8767505be4
Emily Jane McTavish <ejmctavish@gmail.com>
2020-09-21 15:25:18 -0700

    m2r

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ release = u'0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'm2r',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
 ]
