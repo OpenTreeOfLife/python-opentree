658c92527bb93d7b7f6f1902edafac1738ec5aa9
Emily Jane McTavish <ejmctavish@gmail.com>
2020-09-21 13:53:53 -0700

    no args to about

--- examples/about.py
+++ examples/about.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
+"""Get information on current taxonomy and synthesis tree"""
 
 import sys
+import json
 from opentree import OTCommandLineTool
 
 def main(arg_list, out, list_for_results=None):
     cli = OTCommandLineTool(usage='Display taxonomy and synthetic tree information '
                               'returned by the "about" API calls.')
-    OT = cli.parse_cli(arg_list)[0]
+    OT = cli.parse_cli()[0]
     about = OT.about()
     if list_for_results is not None:
         list_for_results.append(about)
@@ -16,7 +18,8 @@ def main(arg_list, out, list_for_results=None):
         call_record = about[k]
         if call_record:
             print(k)
-            call_record.write_response(out)
+            sf = json.dumps(call_record, sort_keys=True, indent=2, separators=(',', ': '), ensure_ascii=True)
+            print(sf)
             print('')
     return 0
 
