b15a118d5a6c9bacdf127912105fca4206b5cc8f
Mark T. Holder <mtholder@gmail.com>
2021-05-04 13:28:25 -0500

    bump of version to 1.0.0

--- opentree/__init__.py
+++ opentree/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-__version__ = "0.0.4"  # sync with setup.py
+__version__ = "1.0.0"  # sync with setup.py
 
 from .object_conversion import get_object_converter
 from .ws_wrapper import (OTWebServicesError,
