257ebe216a2170b81a18da85faa44e767d1ff7a1
Mark T. Holder <mtholder@gmail.com>
2023-07-19 13:19:55 -0500

    args to run_test.sh passed to test runner

--- run_tests.sh
+++ run_tests.sh
@@ -6,12 +6,13 @@ else
     hd=$(dirname $0)
     rhd=$(realpath "${hd}")
     echo "rhd=${rhd}"
-    if ! test -d "${arg}/testopentree" ; then
-        echo "directory ${arg}/testopentree does not exist"
+    if ! test -d "${arg}/opentree" ; then
+        echo "directory ${arg}/opentree does not exist"
         exit 1
     else
+        shift
         export PYTHON_OPENTREE_DIR="${rhd}"
-        nose2 opentree testopentree opentree --coverage opentree
+        nose2 opentree testopentree opentree --coverage opentree $@
     fi
 fi
 
