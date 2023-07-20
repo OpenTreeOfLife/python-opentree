0ca628d88c07b4ca89f0ae932ccfef090b3c2f98
snacktavish <ejmctavish@gmail.com>
2023-07-20 15:48:50 -0700

    more annottation

--- opentree/annotations.py
+++ opentree/annotations.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import dendropy
+import random
 
 
 from opentree import OT
@@ -19,7 +20,12 @@ def debug(msg):
         print(msg)
 
 
-def generate_custom_synth_node_annotation(tree, custom_synth_dir):
+def generate_random_HEX_color():
+    chars = '0123456789ABCDEF'
+    color = '#'+''.join(random.sample(chars,6))
+    return color
+
+def generate_custom_synth_node_annotation(tree, custom_synth_dir, exclude_sources = None):
     """inputs:
     tree
     Tree in dendropy format. must be labelled with ott_ids, and internal node labels
@@ -34,23 +40,23 @@ def generate_custom_synth_node_annotation(tree, custom_synth_dir):
         elif node.taxon:
             if node.taxon.label:
                 node_annotation[node.taxon.label] = {}
-    for node_label in node_annotation:
-        assert node_label.startswith('ott') or node_label.startswith('mrca')
-        node_annotation[node_label] = {}
-        node_annotation[node_label]['studies'] = []
-        node_annotation[node_label]['strict_support'] = []
-        node_annotation[node_label]['support'] = []
-        node_annotation[node_label]['conflict'] = []
     node_ids = node_annotation.keys()
     annot = json.load(open("{}/annotated_supertree/annotations.json".format(custom_synth_dir)))
     node_id_resp = annot['nodes']
+    if exclude_sources:
+        for source in exclude_sources:
+            for node in node_id_resp:
+                for typ in ['supported_by', 'strict_support','partial_path_of','conflict', 'terminal']:
+                    if source in node_id_resp[node].get(typ,{}):
+                        del node_id_resp[node][typ][source]
     for node in node_annotation:
         node_info = node_id_resp.get(node,{})
         strict_support = node_info.get('supported_by', {})
         ppo = node_info.get('partial_path_of', {})
+        term = node_info.get('terminal', {})
         conflict = node_info.get('conflicts_with', {})
         node_annotation[node]['strict_support'] = len(strict_support.keys())
-        gen_support = set(list(strict_support.keys()) + list(ppo.keys()))
+        gen_support = set(list(strict_support.keys()) + list(ppo.keys())  + list(term.keys()))
         node_annotation[node]['support'] = len(gen_support)
         node_annotation[node]['conflict'] = len(conflict)
     return node_annotation
@@ -70,19 +76,14 @@ def generate_custom_synth_source_traversal(tree, custom_synth_dir, source):
         elif node.taxon:
             if node.taxon.label:
                 node_annotation[node.taxon.label] = {}
-    for node_label in node_annotation:
-        assert node_label.startswith('ott') or node_label.startswith('mrca')
-        node_annotation[node_label] = {}
-        node_annotation[node_label]['resolves'] = []
-        node_annotation[node_label]['support'] = []
-        node_annotation[node_label]['conflict'] = []
     node_ids = node_annotation.keys()
     annot = json.load(open("{}/annotated_supertree/annotations.json".format(custom_synth_dir)))
     node_id_resp = annot['nodes']
     for node in node_annotation:
         node_info = node_id_resp.get(node,{})
         strict_support = node_info.get('supported_by', {})
-        if source in strict_support.keys():
+        terminal_of = node_info.get('terminal', {})
+        if source in strict_support.keys() or source in terminal_of.keys():
             node_annotation[node]['support'] = 1
         else:
             node_annotation[node]['support'] = 0
@@ -355,7 +356,7 @@ def write_itol_heatmap(filename, title, unit, node_annotation, param):
 
 
 
-def write_itol_support(node_annotation, max_support=5, filename='support_anno.txt', param='studies'):
+def write_itol_support(node_annotation, title = "support", max_support=5, filename='support_anno.txt', param='studies'):
     """Write out an itol branch support file to filename, with title and units label.
     annot dict must have keys which are node ids in tree on itol (may require underscore space subs..)
     param should be key of annot_dict[node_ids] = {}"""
@@ -369,7 +370,7 @@ def write_itol_support(node_annotation, max_support=5, filename='support_anno.tx
     #dataset color (can be changed later)
     COLOR\t#ffff00
 
-    DATA\n""".format("Support")
+    DATA\n""".format(title)
     fi.write(startstr)
     for node in node_annotation:
         if node_annotation[node][param]:
@@ -385,9 +386,7 @@ def write_itol_support(node_annotation, max_support=5, filename='support_anno.tx
     fi.close()
 
 
-
-
-def write_itol_conflict(node_annotation, max_conflict=5, filename='conflict_anno.txt', param='conflict'):
+def write_itol_conflict(node_annotation, title = "conflict", max_conflict=5, filename='conflict_anno.txt', param='conflict'):
     """Write out an itol branch support file to filename, with title and units label.
     annot dict must have keys which are node ids in tree on itol (may require underscore space subs..)
     param should be key of annot_dict[node_ids] = {}"""
@@ -401,36 +400,7 @@ def write_itol_conflict(node_annotation, max_conflict=5, filename='conflict_anno
     #dataset color (can be changed later)
     COLOR\t#ffff00
 
-    DATA\n""".format("conflict")
-    fi.write(startstr)
-    for node in node_annotation:
-        relconf = node_annotation[node][param]/max_conflict
-        r = 255*relconf
-        g = 0
-        b = 0
-        color = "rgba({}, {}, {}, {})".format(r, g, b, 0.25+relconf)
-        fi.write("{}\tbranch\tclade\t{}\t1\tnormal\n".format(node,color))            
-    fi.close()
-
-
-
-
-
-def write_itol_conflict(node_annotation, max_conflict=5, filename='conflict_anno.txt', param='conflict'):
-    """Write out an itol branch support file to filename, with title and units label.
-    annot dict must have keys which are node ids in tree on itol (may require underscore space subs..)
-    param should be key of annot_dict[node_ids] = {}"""
-    fi = open(filename, 'w')
-    startstr = """DATASET_STYLE
-    SEPARATOR TAB
-
-    #label is used in the legend table (can be changed later)
-    DATASET_LABEL\t{}
-
-    #dataset color (can be changed later)
-    COLOR\t#ffff00
-
-    DATA\n""".format("conflict")
+    DATA\n""".format(title)
     fi.write(startstr)
     for node in node_annotation:
         relconf = node_annotation[node][param]/max_conflict
@@ -470,4 +440,157 @@ def write_itol_relabel(translation_dict, filename):
 
     for current_label in translation_dict:
         fi.write("{},{}\n".format(current_label, translation_dict[current_label]))
+    fi.close()
+
+
+
+
+def write_itol_clades(node_label_dict, filename, font_size=4):
+    """Keys in translation dict should be current labels, values new labels"""
+    fi = open(filename, 'w')
+    startstr = """DATASET_RANGE
+    #Colored/labeled range datasets allow the highlighting of various clades or leaf ranges by using colored boxes or brackets.
+
+    #=================================================================#
+    #                    MANDATORY SETTINGS                           #
+    #=================================================================#
+    #select the separator which is used to delimit the data below (TAB,SPACE or COMMA).This separator must be used throughout this file.
+
+    SEPARATOR COMMA
+
+    #label is used in the legend table (can be changed later)
+    DATASET_LABEL, Range
+
+    #dataset color in the legend table
+    COLOR,#ffff00
+
+
+    #=================================================================#
+    #                    OPTIONAL SETTINGS                            #
+    #=================================================================#
+
+    #=================================================================#
+    #        all optional settings can be set or changed later        #
+    #           in the web interface (under 'Datasets' tab)           #
+    #=================================================================#
+
+    #RANGE_TYPE defines how the rages will be visualized:
+       #box: standard colored box/polygon. Various LINE_? fields in the range definition will be used for the border style. 
+       #bracket: a line or bracket outside the tree
+
+    RANGE_TYPE,box
+    #
+    #Box/polygon specific options, used when RANGE_TYPE is 'box'
+    #
+
+    #specify what the range boxes will cover: 'label','clade' or 'tree'
+    RANGE_COVER,clade
+
+    #simplify or smooth polygons when in unrooted display mode: 'none', 'simplify' or 'smooth'
+    UNROOTED_SMOOTH,simplify
+
+    #when RANGE_COVER is set to 'clade' or 'tree', you can disable the covering of labels (ie. limiting the boxes to the tree structure only)
+    COVER_LABELS,0
+
+    #if set to 1, ranges will cover any displayed extrernal datasets as well
+    COVER_DATASETS,0
+
+    #if set to 1, size of the boxes will be extended to fit their labels
+    FIT_LABELS,0
+
+    #
+    #Bracket specific options, used when RANGE_TYPE is 'bracket'
+    #
+
+    #bracket style can be: 'none','square' or 'curved'
+    BRACKET_STYLE,square
+
+    #size of the bracket ends (for 'square' or 'curved' brackets)
+    BRACKET_SIZE,20
+
+    #shift the bracket position horizontally
+    BRACKET_SHIFT,50
+
+    #if set to 1, brackets will be displayed behind the last visible external dataset
+    BRACKET_BEHIND_DATASETS,1
+
+    #
+    #Options related to range labels
+    #
+
+    SHOW_LABELS,1
+
+    #the position of the label in the range box (or relative to the bracket): 'top-left','top-center','top-right',
+    #                                                                         'center-left','center-center','center-right',
+    #                                                                         'bottom-left','bottom-center','bottom-right'
+    LABEL_POSITION,center-center
+
+    #Display the labels vertically. In circular display mode (or with brackets in unrooted display mode), labels will be aligned to the circle
+    LABELS_VERTICAL,0
+
+    #labels remain straight, regardless of the tree rotation or other rotation parameters
+    STRAIGHT_LABELS,0
+
+    #rotate all labels by the specified angle
+    LABEL_ROTATION,0
+
+    #shift all labels horizontally and/or vertically
+    LABEL_SHIFT_X,0
+    LABEL_SHIFT_Y,0
+
+    #add a colored outline to the label font; useful when displaying labels over similarly colored boxes (e.g. black font on a dark box)
+    LABEL_OUTLINE_WIDTH,1
+    LABEL_OUTLINE_COLOR,#ffffff
+
+    #multiply the size of all labels by this factor
+    LABEL_SIZE_FACTOR,1
+
+
+    #Each dataset can have a legend, which is defined using LEGEND_XXX fields below
+    #For each row in the legend, there should be one shape, color and label.
+    #Optionally, you can define an exact legend position using LEGEND_POSITION_X and LEGEND_POSITION_Y. To use automatic legend positioning, do NOT define these values
+    #Optionally, shape scaling can be present (LEGEND_SHAPE_SCALES). For each shape, you can define a scaling factor between 0 and 1.
+    #To order legend entries horizontally instead of vertically, set LEGEND_HORIZONTAL to 1
+    #Shape should be a number between 1 and 6, or any protein domain shape definition.
+    #1: square
+    #2: circle
+    #3: star
+    #4: right pointing triangle
+    #5: left pointing triangle
+    #6: checkmark
+
+    #LEGEND_TITLE,Dataset legend
+    #LEGEND_POSITION_X,100
+    #LEGEND_POSITION_Y,100
+    #LEGEND_HORIZONTAL,0
+    #LEGEND_SHAPES,1,4,3
+    #LEGEND_COLORS,#ff0000,#00ff00,#0000ff
+    #LEGEND_LABELS,value1,value2,value3
+    #LEGEND_SHAPE_SCALES,1,1,0.5
+
+
+
+    #Internal tree nodes can be specified by using IDs directly, or by using the 'last common ancestor' method described in iTOL help pages
+    #=================================================================#
+    #       Actual data follows after the "DATA" keyword              #
+    #=================================================================#
+    #the following fields are available in each line:
+
+    #START_NODE_ID,END_NODE_ID,FILL_COLOR,GRADIENT_COLOR,LINE_COLOR,LINE_STYLE,LINE_WIDTH,LABEL_TEXT,LABEL_COLOR,LABEL_SIZE_FACTOR,LABEL_STYLE
+
+    #The range is defined through START_NODE_ID and END_NODE_ID.
+    #If GRADIENT_FILL color is defined, the box will be filled with a gradient from FILL_COLOR to GRADIENT_COLOR.  Brackets will also be visualized as gradients.
+    #LINE_COLOR will be used for the box/polygon border, or for the brackets. If not specified, FILL_COLOR will be used instead
+    #LINE_STYLE can be 'solid', 'dashed' or 'dotted'
+    #LABEL_STYLE can be 'normal', 'bold', 'italic' or 'bold-italic'
+
+    DATA
+    #Examples\n"""
+    fi.write(startstr)
+
+
+    for node in node_label_dict:
+        color =  generate_random_HEX_color()
+        clade_label = node_label_dict[node]
+        fi.write("{n},{n},#ffffff,{c},#000000,solid,0,{cl},#000000,{f},bold\n".format(n=node, c=color, f= font_size, cl=clade_label))
     fi.close()
